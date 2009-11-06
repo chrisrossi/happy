66d2f62b9ab4178d3856ea30229e67a79a593c67
Chris Rossi <chris@archimedeanco.com>
2009-11-05 23:26:15 -0500

    Module level documentation.

--- happy/traversal.py
+++ happy/traversal.py
@@ -1,6 +1,54 @@
 """
-Handles the notion of dispatching to a view (aka controller) based on traversal
-of an object graph.
+This module provides tools for dispatching to controllers based on traversal of
+an object graph.  The object graph is populated with the data objects, called
+models, that are exposed by your web site.  The graph has a tree structure
+which maps onto the url being requested.  Each model object represents the
+content you want to expose via your web site.
+
+Consider, for illustration purposes, the following classes::
+
+    class Folder(dict):
+        pass
+
+    class Cat(object):
+        pass
+
+    class Dog(object):
+        pass
+
+Now let's presume instances of these classes have been stored somewhere and
+composed into following object graph::
+
+    root = Folder()
+    cats = root['cats'] = Folder()
+    lily = cats['lily'] = Cat()
+    ginger = cats['ginger'] = Cat()
+    dogs = root['dogs'] = Folder()
+    elsa = dogs['elsa'] = Dog()
+    nemo = dogs['nemo'] = Dog()
+
+The following urls would then map onto the above objects::
+
+    /            (root folder)
+    /cats/       (cats folder)
+    /cats/lily   (lily cat)
+    /dogs/elsa   (elsa dog)
+
+The object graph and corresponding url space represent a natural containment
+hierarchy and is an intuitive way to organize content in content management
+type applications, where content can be organized in arbitrarily nested
+structures.
+
+Traversal of the object hierarchy is done using only the ``__getitem__`` method
+of model objects.  A model which has the ``__getitem__`` method is a container
+and may contain other models.  A model without the ``__getitem__`` method is,
+necessarily, a leaf node.  This is the only contract expected of models in
+order to be traversable.
+
+In order for anything interesting to happen, models must be mapped to
+conrollers in some way. For information on mapping models to controllers, see
+the ``TraversalDispatcher`` class.
+
 """
 
 from happy.registry import Registry
@@ -32,6 +80,7 @@ def traverse(root, path):
 
 class TraversalDispatcher(object):
     """
+    XXX
     """
     def __init__(self, root_factory, registry=None):
         self.root_factory = root_factory
@@ -48,9 +97,7 @@ class TraversalDispatcher(object):
 
     def _lookup_controller(self, context, request, subpath):
         """
-        Specifically put here as an override point for subclasses that might
-        want to do something different to find a controller for a specific
-        context.
+        XXX
         """
         if subpath:
             return self.registry.lookup(context, subpath[0])
