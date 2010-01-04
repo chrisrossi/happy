28e43be1d4dfe18fa2b4808c60da23ea4a5cc3ad
Chris Rossi <chris@archimedeanco.com>
2010-01-03 20:22:44 -0500

    Integration testing for reference implementations.

--- happy/tests/test_login.py
+++ happy/tests/test_login.py
@@ -1,13 +1,72 @@
 import unittest
 
+class DummyPasswordBroker(object):
+    _passwords = {
+        'chris@example.com': '12345678',
+    }
+
+    def __call__(self, login, password):
+        return login in self._passwords and self._passwords[login] == password
+
+class DummyPrincipalsBroker(object):
+    _principals = {
+        'chris@example.com': ['user-1234', 'group.Administrators']
+    }
+
+    def get_userid(self, login):
+        return self._principals[login][0]
+
+    def get_principals(self, login):
+        return self._principals[login]
+
+class DummyCredentialBroker(object):
+    def __init__(self):
+        self._credentials = {}
+
+    def login(self, login):
+        import uuid
+        credential = str(uuid.uuid4())
+        self._credentials[credential] = login
+        return credential
+
+    def logout(self, credential):
+        del self._credentials[credential]
+
+    def get_login(self, credential):
+        return self._credentials.get(credential, None)
+
+class DummyResponse(object):
+    def __init__(self, status_int=200):
+        self.status_int = status_int
+
+def dummy_app(request):
+    response = DummyResponse()
+    response.remote_user = request.remote_user
+    response.principals = getattr(
+        request, 'authenticated_principals', None
+    )
+    return response
+
+def get_cookie(response, cookie_name):
+    for k, v in response.headers.items():
+        if k == 'Set-Cookie':
+            name, value = v.split(';')[0].split('=')
+            if name == cookie_name:
+                return value
+
+
 class TestFormLoginMiddleware(unittest.TestCase):
+    PasswordBroker = DummyPasswordBroker
+    PrincipalsBroker = DummyPrincipalsBroker
+    CredentialBroker = DummyCredentialBroker
+
     def _make_one(self, app, **kw):
         from happy.login import FormLoginMiddleware
         return FormLoginMiddleware(
             app,
-            DummyPasswordBroker(),
-            DummyPrincipalsBroker(),
-            DummyCredentialBroker(),
+            self.PasswordBroker(),
+            self.PrincipalsBroker(),
+            self.CredentialBroker(),
             **kw
         )
 
@@ -83,10 +142,11 @@ class TestFormLoginMiddleware(unittest.TestCase):
         fut = self._make_one(dummy_app)
         request = Request.blank(
             '/login',
-            POST=dict(login='chris@example.com', password='123456789')
+            POST=dict(login='chris@example.com', password='adfjdfkjk')
         )
-        body = fut(request).body
-        self.failUnless('Bad login' in body, body)
+        response = fut(request)
+        self.assertEqual(response.status_int, 200)
+        self.failUnless('Bad login' in response.body, response.body)
 
     def test_redirect_401(self):
         def app(request):
@@ -129,7 +189,7 @@ class TestHtpasswdAuthenticator(unittest.TestCase):
     def setUp(self):
         import os
         import tempfile
-        fd, self.htpasswd_file = tempfile.mkstemp('.lever.auth.test')
+        fd, self.htpasswd_file = tempfile.mkstemp('.happy.test')
         os.close(fd)
 
         with open(self.htpasswd_file, 'w') as f:
@@ -168,7 +228,7 @@ class TestFlatFilePrincipalsBrokerTests(unittest.TestCase):
     def setUp(self):
         import os
         import tempfile
-        self.fname = tempfile.mktemp('.lever.auth.test')
+        self.fname = tempfile.mktemp('.happy.test')
 
         with open(self.fname, 'w') as f:
             print >>f, "# temp file for testing"
@@ -214,70 +274,65 @@ class TestRandomUUIDCredentialBroker(unittest.TestCase):
         tmpdir = tempfile.mkdtemp('_happy_test')
         db_file = os.path.join(tmpdir, 'credentials.db')
 
+        try:
+            from happy.login import RandomUUIDCredentialBroker
+            broker = RandomUUIDCredentialBroker(db_file)
+            credential = broker.login('fumanchu')
+            self.assertEqual(broker.get_login(credential), 'fumanchu')
+
+            broker = RandomUUIDCredentialBroker(db_file)
+            self.assertEqual(broker.get_login(credential), 'fumanchu')
+
+            broker.logout(credential)
+            self.assertEqual(broker.get_login(credential), None)
+
+            broker = RandomUUIDCredentialBroker(db_file)
+            self.assertEqual(broker.get_login(credential), None)
+
+        finally:
+            import shutil
+            shutil.rmtree(tmpdir)
+
+class TestRefImplIntegration(TestFormLoginMiddleware):
+    def setUp(self):
+        import os
+        import tempfile
+        tmpdir = tempfile.mkdtemp('_happy_test')
+        self.principals_db = os.path.join(tmpdir, 'principals')
+
+        with open(self.principals_db, 'w') as f:
+            print >>f, "# temp file for testing"
+            print >>f, ""
+        self._add_user_principals('chris@example.com', [
+            'user-1234', 'group.Administrators'
+            ]
+        )
+
+        self.htpasswd_file = os.path.join(tmpdir, 'htpasswd')
+        with open(self.htpasswd_file, 'w') as f:
+            print >>f, "# temp file for testing"
+        self._add_user_password('chris@example.com', '12345678')
+
+    def _add_user_principals(self, login, principals):
+        import crypt
+        principals = ', '.join(principals).encode('utf-8')
+        with open(self.principals_db, 'a') as f:
+            print >>f, '%s: %s' % (login, principals)
+
+
+    def _add_user_password(self, login, passwd):
+        import crypt
+        with open(self.htpasswd_file, 'a') as f:
+            print >>f, '%s:%s' % (login, crypt.crypt(passwd, 'salt'))
+
+    def PasswordBroker(self):
+        from happy.login import HtpasswdBroker
+        return HtpasswdBroker(self.htpasswd_file)
+
+    def PrincipalsBroker(self):
+        from happy.login import FlatFilePrincipalsBroker
+        return FlatFilePrincipalsBroker(self.principals_db)
+
+    def CredentialBroker(self):
         from happy.login import RandomUUIDCredentialBroker
-        broker = RandomUUIDCredentialBroker(db_file)
-        credential = broker.login('fumanchu')
-        self.assertEqual(broker.get_login(credential), 'fumanchu')
-
-        broker = RandomUUIDCredentialBroker(db_file)
-        self.assertEqual(broker.get_login(credential), 'fumanchu')
-
-        broker.logout(credential)
-        self.assertEqual(broker.get_login(credential), None)
-
-        broker = RandomUUIDCredentialBroker(db_file)
-        self.assertEqual(broker.get_login(credential), None)
-
-class DummyPasswordBroker(object):
-    _passwords = {
-        'chris@example.com': '12345678',
-    }
-
-    def __call__(self, login, password):
-        return login in self._passwords and self._passwords[login] == password
-
-class DummyPrincipalsBroker(object):
-    _principals = {
-        'chris@example.com': ['user-1234', 'group.Administrators']
-    }
-
-    def get_userid(self, login):
-        return self._principals[login][0]
-
-    def get_principals(self, login):
-        return self._principals[login]
-
-class DummyCredentialBroker(object):
-    def __init__(self):
-        self._credentials = {}
-
-    def login(self, login):
-        import uuid
-        credential = str(uuid.uuid4())
-        self._credentials[credential] = login
-        return credential
-
-    def logout(self, credential):
-        del self._credentials[credential]
-
-    def get_login(self, credential):
-        return self._credentials.get(credential, None)
-
-class DummyResponse(object):
-    def __init__(self, status_int=200):
-        self.status_int = status_int
-
-def dummy_app(request):
-    response = DummyResponse()
-    response.remote_user = request.remote_user
-    response.principals = getattr(
-        request, 'authenticated_principals', None
-    )
-    return response
-
-def get_cookie(response, cookie_name):
-    for k, v in response.headers.items():
-        if k == 'Set-Cookie':
-            name, value = v.split(';')[0].split('=')
-            if name == cookie_name:
-                return value
+        return RandomUUIDCredentialBroker()
