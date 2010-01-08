1877b3baefe8850ca750af772305ceee8d97b6eb
Chris Rossi <chris@archimedeanco.com>
2010-01-08 09:25:07 -0500

    Work around bogus assert in WebOb.Request.__get_body

--- happy/tests/test_static.py
+++ happy/tests/test_static.py
@@ -84,15 +84,17 @@ class TestFileResponse(unittest.TestCase):
         request = webob.Request.blank('/')
         request.headers['Range'] = 'bytes=0-99'
         response = FileResponse(fpath, request)
-        self.assertEqual(len(response.body), 100)
-        self.assertEqual(ord(response.body[0]), 0)
+        body = ''.join(list(response.app_iter))
+        self.assertEqual(len(body), 100)
+        self.assertEqual(ord(body[0]), 0)
         self.assertEqual(response.status, '206 Partial Content')
 
         request = webob.Request.blank('/')
         request.headers['Range'] = 'bytes=-200'
         response = FileResponse(fpath, request)
-        self.assertEqual(len(response.body), 200)
-        self.assertEqual(ord(response.body[0]), 600 % 0x100)
+        body = ''.join(list(response.app_iter))
+        self.assertEqual(len(body), 200)
+        self.assertEqual(ord(body[0]), 600 % 0x100)
         self.assertEqual(response.status_int, 206)
 
         expected = open(fpath, 'rb').read()
@@ -105,7 +107,9 @@ class TestFileResponse(unittest.TestCase):
         for i in xrange(len(ranges)):
             request = webob.Request.blank('/')
             request.headers['Range'] = ranges[i]
-            ranges[i] = FileResponse(fpath, request).body
+            response = FileResponse(fpath, request)
+            body = ''.join(list(response.app_iter))
+            ranges[i] = body
         got = ''.join(ranges)
 
         self.assertEqual(got, expected)
