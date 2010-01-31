33cf99144b63b5bc918a6e841143d94655e8074f
Chris Rossi <chris@archimedeanco.com>
2010-01-31 18:31:52 -0500

    New and improved registry.

--- happy/registry.py
+++ happy/registry.py
@@ -85,57 +85,136 @@ class Registry(object):
     Implements registry.  See module level documentation.
     """
     def __init__(self, *axes):
-        self._registry = _MapNode()
-        self.axes = axes
+        self._tree = _TreeNode()
+        self._axes = [axis for name, axis in axes]
+        self._axes_dict = dict([
+            (name, (i, axis)) for i, (name, axis) in enumerate(axes)
+        ])
 
-    def register(self, target, *keys):
-        if len(keys) > len(self.axes):
-            raise ValueError("Can't register more keys than there are axes.")
+    def register(self, target, *arg_keys, **kw_keys):
+        self._register(target, self._align_with_axes(arg_keys, kw_keys), False)
 
-        map_node = self._registry
+    def override(self, target, *arg_keys, **kw_keys):
+        self._register(target, self._align_with_axes(arg_keys, kw_keys), True)
+
+    def _register(self, target, keys, override):
+        tree_node = self._tree
         for key in keys:
-            if not map_node.has_key(key):
-                map_node[key] = _MapNode()
-            map_node = map_node[key]
-        map_node.target = target
+            if not tree_node.has_key(key):
+                tree_node[key] = _TreeNode()
+            tree_node = tree_node[key]
 
-    def lookup(self, *objs):
-        if len(objs) > len(self.axes):
-            raise ValueError("Can't look up more keys than there are axes.")
+        if not (override or tree_node.target is None):
+            raise ValueError(
+                "Registration conflicts with existing registration.  Use "
+                "override method to override."
+            )
 
-        map_node = self._registry
-        keys = map(lambda x: x[0](x[1]), zip(self.axes, objs))
+        tree_node.target = target
 
-        return self._lookup(map_node, self.axes, keys)
+    def lookup(self, *arg_objs, **kw_objs):
+        objs = self._align_with_axes(arg_objs, kw_objs)
+        axes = self._axes
+        return self._lookup(self._tree, objs, axes)
 
-    def _lookup(self, map_node, axes, keys):
-        if not keys:
-            return getattr(map_node, 'target', None)
+    def _lookup(self, tree_node, objs, axes):
+        """
+        Recursively traverse registration tree, from left to right, most
+        specific to least specific, returning the first target found on a
+        matching node.
+        """
+        if not objs:
+            return tree_node.target
 
-        axis, key = axes[0], keys[0]
-        if getattr(axis, 'specificity', False):
-            for k in key:
-                if k in map_node:
-                    target = self._lookup(map_node[k], axes[1:], keys[1:])
-                    if target is not None:
-                        return target
+        obj = objs[0]
 
-        elif key in map_node:
-            return self._lookup(map_node[key], axes[1:], keys[1:])
+        # Skip non-participating nodes
+        if obj is None:
+            next_node = tree_node.get(None, None)
+            if next_node is not None:
+                return self._lookup(next_node, objs[1:], axes[1:])
+            return None
+
+        # Get matches on this axis and iterate from most to least specific
+        axis = axes[0]
+        for match_key in axis.matches(obj, tree_node.keys()):
+            target = self._lookup(tree_node[match_key], objs[1:], axes[1:])
+            if target is not None:
+                return target
 
         return None
 
+    def _align_with_axes(self, args, kw):
+        """
+        Create a list matching up all args and kwargs with their corresponding
+        axes, in order, using 'None' as a placeholder for skipped axes.
+        """
+        axes_dict = self._axes_dict
+        aligned = [None for i in xrange(len(axes_dict))]
 
-class _MapNode(dict):
+        args_len = len(args)
+        if args_len + len(kw) >  len(aligned):
+            raise ValueError('Cannot have more arguments than axes.')
+
+        for i, arg in enumerate(args):
+            aligned[i] = arg
+
+        for k, v in kw.items():
+            i_axis = axes_dict.get(k, None)
+            if i_axis is None:
+                raise ValueError('No axis with name: %s' % k)
+
+            i, axis = i_axis
+            if aligned[i] is not None:
+                raise ValueError('Axis defined twice between positional and '
+                                 'keyword arguments')
+
+            aligned[i] = v
+
+        # Trim empty tail nodes, for somewhat faster look ups
+        while aligned and aligned[-1] is None:
+            del aligned[-1]
+
+        return aligned
+
+class _TreeNode(dict):
     target = None
 
+class BaseAxis(object):
+    """
+    Provides a convenient base with abstract methods to be overridden by axis
+    implementations. Provides a good, basic implementation of 'matches' that
+    is factored to include most common cases. More complex algorithms should
+    probably just implement their own class from the ground up.
+    """
+    def matches(self, obj, keys):
+        for key in self.get_keys(obj):
+            if key in keys:
+                yield key
 
-class MROAxis(object):
-    specificity = True
+    def get_keys(self, obj):
+        """
+        For a given object, return an iteratable of the keys that can possibly
+        be used to match this object in the axis, from most specific to least
+        specific.
+        """
+        raise NotImplementedError("Must be implemented by concrete subclass.")
 
-    def __call__(self, obj):
+class SimpleAxis(BaseAxis):
+    """
+    A simple axis where the key into the axis is the same as the object to be
+    matched (aka the identity axis). This axis behaves just like a dictionary.
+    You might use this axis if you are interested in registering something by
+    name, where you're registering an object with the string that is the name
+    and then using the name to look it up again later.
+    """
+    def get_keys(self, obj):
+        return [obj,]
+
+class TypeAxis(BaseAxis):
+    """
+    An axis which matches the class and super classes of an object in method
+    resolution order.
+    """
+    def get_keys(self, obj):
         return type(obj).mro()
-
-
-mro_axis = MROAxis()
-identity_axis = lambda x: x
--- happy/tests/test_registry.py
+++ happy/tests/test_registry.py
@@ -3,8 +3,8 @@ import unittest
 class RegistryTests(unittest.TestCase):
     def test_one_axis_no_specificity(self):
         from happy.registry import Registry
-        from happy.registry import identity_axis
-        registry = Registry(identity_axis)
+        from happy.registry import SimpleAxis
+        registry = Registry(('foo', SimpleAxis()))
         a = object()
         b = object()
         registry.register(a)
@@ -16,9 +16,10 @@ class RegistryTests(unittest.TestCase):
 
     def test_two_axes(self):
         from happy.registry import Registry
-        from happy.registry import identity_axis
-        from happy.registry import mro_axis
-        registry = Registry(mro_axis, identity_axis)
+        from happy.registry import SimpleAxis
+        from happy.registry import TypeAxis
+        registry = Registry(('type', TypeAxis()),
+                            ('name', SimpleAxis()))
 
         target1 = Target('one')
         registry.register(target1, object)
@@ -44,16 +45,68 @@ class RegistryTests(unittest.TestCase):
 
     def test_register_too_many_keys(self):
         from happy.registry import Registry
-        from happy.registry import identity_axis
-        registry = Registry(identity_axis)
+        from happy.registry import SimpleAxis
+        registry = Registry(('name', SimpleAxis()))
         self.assertRaises(ValueError, registry.register, object(),
                           'one', 'two')
 
     def test_lookup_too_many_keys(self):
         from happy.registry import Registry
-        from happy.registry import identity_axis
-        registry = Registry(identity_axis)
+        from happy.registry import SimpleAxis
+        registry = Registry(('name', SimpleAxis()))
         self.assertRaises(ValueError, registry.lookup, 'one', 'two')
+
+    def test_conflict_error(self):
+        from happy.registry import Registry
+        from happy.registry import SimpleAxis
+        registry = Registry(('name', SimpleAxis()))
+        registry.register(object(), name='foo')
+        self.assertRaises(ValueError, registry.register, object(), 'foo')
+
+    def test_override(self):
+        from happy.registry import Registry
+        from happy.registry import SimpleAxis
+        registry = Registry(('name', SimpleAxis()))
+        registry.register(1, name='foo')
+        registry.override(2, name='foo')
+        self.assertEqual(registry.lookup('foo'), 2)
+
+    def test_skip_nodes(self):
+        from happy.registry import Registry
+        from happy.registry import SimpleAxis
+        registry = Registry(
+            ('one', SimpleAxis()),
+            ('two', SimpleAxis()),
+            ('three', SimpleAxis())
+            )
+        registry.register('foo', one=1, three=3)
+        self.assertEqual(registry.lookup(1, three=3), 'foo')
+
+    def test_miss(self):
+        from happy.registry import Registry
+        from happy.registry import SimpleAxis
+        registry = Registry(
+            ('one', SimpleAxis()),
+            ('two', SimpleAxis()),
+            ('three', SimpleAxis())
+            )
+        registry.register('foo', 1, 2)
+        self.assertEqual(registry.lookup(one=1, three=3), None)
+
+    def test_bad_lookup(self):
+        from happy.registry import Registry
+        from happy.registry import SimpleAxis
+        registry = Registry(('name', SimpleAxis()),
+                            ('grade', SimpleAxis()))
+        self.assertRaises(ValueError, registry.register, 1, foo=1)
+        self.assertRaises(ValueError, registry.lookup, foo=1)
+        self.assertRaises(ValueError, registry.register, 1, 'foo', name='foo')
+
+class TestBaseAxis(unittest.TestCase):
+    def test_abstract_method(self):
+        from happy.registry import BaseAxis
+        axis = BaseAxis()
+        self.assertRaises(NotImplementedError, axis.get_keys, None)
 
 class DummyA(object):
     pass
